7a17ba47aa16cd9781d58777e40d7f20d6551974
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-10-22 17:31:00 +0200

    Renames scripts

--- annonces-immo.sh
+++ annonces-immo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-node annonces-ca-immo.js
-node annonces-swisslife-immo.js
+node credit-agricole-immo.js
+node swisslife-immo.js
 node agencepereire.js
