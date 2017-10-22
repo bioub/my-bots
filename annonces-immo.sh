72607406e15457e85a76bc4e270f297f5ed74443
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-10-22 17:03:48 +0200

    New script multi-plateforme

--- annonces-immo.sh
+++ annonces-immo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export PATH=/usr/local/bin:$PATH
-cd /Users/romain/Bots
-/Users/romain/Bots/node_modules/casperjs/bin/casperjs annonces-ca-immo.js
-/Users/romain/Bots/node_modules/casperjs/bin/casperjs annonces-swisslife-immo.js
-/usr/local/bin/node agencepereire.js
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+node annonces-ca-immo.js
+node annonces-swisslife-immo.js
+node agencepereire.js
