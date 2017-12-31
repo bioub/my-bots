00130d3ef85428fb0f2870edc706c427a46e483c
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-12-31 16:59:08 +0100

    Send Annonces throught Slack

--- src/utils/send-annonces.ts
+++ src/utils/send-annonces.ts
@@ -1,21 +1,10 @@
 import { config } from './config';
 import { Annonce } from '../models/annonce';
-
-import { createTransport } from 'nodemailer';
-import * as mailgunTransportFactory from 'nodemailer-mailgun-transport';
-
-const mailer = createTransport(mailgunTransportFactory(config.mailgun));
+import axios from 'axios';
 
 export function sendAnnonces(nomSite: string, annonces: Annonce[]) {
-  const email = {
-    from: `Bot ${nomSite} <postmaster@mail.bioub.com>`,
-    to: config.dest.join(', '),
-    subject: `Nouvelles annonces ${nomSite}`,
-    text: annonces.map(a => a.lien).join('\n'),
-  };
-
-  mailer.sendMail(email, (err, info) => {
-    console.log('err', err);
-    console.log('info', info);
+  axios.post('https://hooks.slack.com/services/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', {
+    text: `*${annonces.length} nouvelle${annonces.length>1?'s':''} annonce${annonces.length>1?'s':''} ${nomSite}*\n` +
+          annonces.map(a => a.lien).join('\n'),
   });
 }
