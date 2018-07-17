b58b3234d5e5f852b983b95c4117e11981981138
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2018-07-17 15:15:02 +0200

    fix: credit agricole script

--- src/scripts/immo/credit-agricole-immo.ts
+++ src/scripts/immo/credit-agricole-immo.ts
@@ -2,7 +2,7 @@ import { getAnnonces } from '../../utils/get-annonces';
 
 getAnnonces('Crédit Agricole Immobilier', async function(page) {
   await page.goto(
-    'https://www.ca-immobilier.fr/louer/recherche?sections=location&codes=75000%3Aparis%2C92200%3Aneuilly%20sur%20seine%2C92300%3Alevallois%20perret&zones=3%2C4%2C5%2Cmore&maxprice=2200&sortby=price_asc',
+    'https://www.ca-immobilier.fr/louer/recherche?sections=location&codes=75%3Aparis&zones=3%2C4%2C5%2Cmore&maxprice=2200&sortby=price_asc',
   );
 
   const annonces = await page.evaluate(() => {
